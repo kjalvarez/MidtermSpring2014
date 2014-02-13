adeda00c5e3c0fde5bd803216fb3fdc773ab334b
Karin <karin.j.alvarez@gmail.com>
2014-02-13 12:29:59 -0700

    Added styling

--- midterm.js
+++ midterm.js
@@ -13,36 +13,36 @@ $(document).on('ready', function() {
 
 	var currentDateDiv = $('#calendar').append(newDiv);
 
-		for (i=0; i<data.length; i++) {
-		var mealInfo = data[i];
-		var mealSource=$('#calendarMealInfo').html();
-		var mealTemplate=Handlebars.compile(mealSource);
-		currentDateDiv = currentDateDiv.append(mealTemplate(mealInfo));
-		}
-	
+  for (i=0; i<data.length; i++) {
+      var mealInfo = data[i];
+      var mealSource=$('#calendarMealInfo').html();
+      var mealTemplate=Handlebars.compile(mealSource);
+      currentDateDiv = currentDateDiv.append(mealTemplate(mealInfo));
+  }
 
-$('#submitButton').on('click', function (e) {
-	e.preventDefault;
-	var requestedDate = ($('#dateInput').val());
-	console.log(requestedDate);
 
-		for(i=0; i<data.length; i++) {
-			if (requestedDate===data[i].date && 
-				data[i].firstName==='') {
-				data[i].firstName=($('#firstNameInput').val());
-				data[i].lastName=($('#lastNameInput').val());
-				data[i].item=($('#itemInput').val());
-				data[i].container=($('#containerInput').val());
-				console.log(data[i]);
+  $('#submitButton').on('click', function (e) {
+   e.preventDefault;
+   var requestedDate = ($('#dateInput').val());
+   console.log(requestedDate);
 
-				
-			} 
-			}
+   for(i=0; i<data.length; i++) {
+     if (requestedDate===data[i].date && 
+        data[i].firstName==='') {
+    data[i].firstName=($('#firstNameInput').val());
+    data[i].lastName=($('#lastNameInput').val());
+    data[i].item=($('#itemInput').val());
+    data[i].container=($('#containerInput').val());
+    console.log(data[i]);
 
-	Write a fx to re-render page with new array . . . ?
+    write function to re-render array . . .
+} 
+}
 
-	}
-		 
+
+
+}
+
 });
 
 
